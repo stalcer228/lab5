0359dec5568e2f9f41cb99451be6c933773f1d31
stalcer <brotherpahom@gmail.com>
2017-04-26 20:36:55 +0700

    1

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,28 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <list>     // подключаем заголовок списка
+#include <iterator> // заголовок итераторов
+#include <ctime>
+#include <cstdlib>
+using namespace std;
 
 int main()
 {
-    return 0;
-}
+	long val;
+	list<long> myList; // объ€вл€ем пустой список
+	srand(time(NULL));
 
+	for (int i = 0; i < 15; i++) {
+		myList.push_back(rand() % 20); // добавл€ем в список новые элементы
+	}
+
+	cout << "—писок: ";
+	copy(myList.begin(), myList.end(), ostream_iterator<long>(cout, " "));
+	
+	cin >> val;
+	myList.remove(val);
+	copy(myList.begin(), myList.end(), ostream_iterator<long>(cout, " "));
+	system("Pause");
+}
